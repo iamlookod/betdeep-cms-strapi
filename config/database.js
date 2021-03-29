22b4d2701af1e562bff7be29a44297ad618d6d91
precharcDev <precharc@scg.com>
2021-03-29 22:18:06 +0700

    Init: update database config

--- config/database.js
+++ config/database.js
@@ -4,12 +4,25 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        schema: env('DATABASE_SCHEMA', 'public'), // Not Required
+        ...(env('NODE_ENV') === 'production' && {
+          ssl: {
+            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+          }
+        })
       },
       options: {
-        useNullAsDefault: true,
-      },
+        ...(env('NODE_ENV') === 'production' && {
+          ssl: env.bool('DATABASE_SSL', false),
+        }),
+        autoMigration: true,
+      }
     },
   },
 });
